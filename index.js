1fd893fdd6c7b44d9dc5843272bad6f955561285
Gabriela Rojas <gabrieladelpilarrojas21@gmail.com>
2022-09-07 09:01:56 -0500

    Leyendo un archivo

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
-module.exports = () => {
-  // ...
-};
+const path = require('node:path');
+const fs = require('node:fs');
+
+//La ruta existe o no 
+const routeExists = fs.existsSync('C:/Users/Usuario/Documents/LIM018/mdn-links/LIM018-md-links/README.md');
+console.log(routeExists)
+
+//La ruta es absoluta o no 
+const routeisAbsolute = (ispath) => {
+    if(path.isAbsolute(ispath)){
+        console.log("La ruta es absoluta")
+        return ispath;
+    }else{
+        console.log("La ruta no es absoluta")
+        return path.resolve(ispath);
+    }
+}
+console.log(routeisAbsolute('C:/Users/Usuario/Documents/LIM018/mdn-links/LIM018-md-links/prueba'));
+
+//Leyendo un archivo
+const readFile = fs.readFileSync('./prueba/link1.md', 'utf-8');
+console.log(readFile);
+
+//Averigua la extensión de un archivo
+const getExt = (file) => path.extname(file);
+console.log(getExt('./prueba/link1.md'))
+
+//Lee un directorio 
+const readDir = (isDir) => {
+    if (isDir){
+    return fs.readdirSync(isDir)
+}}
+console.log(readDir('./prueba'))
+
+//Verifica si la ruta es un directorio
+const pathIsDir = (route) => fs.lstatSync(route).isDirectory();
+console.log(pathIsDir('./prueba'));
+
+const joinRoute = path.join('./prueba','./test');
+console.log(joinRoute)
